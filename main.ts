3a28d11237549d4e325f05b2451aedbc69c2f97f
misteltein <itogahibiki@gmail.com>
2021-01-16 18:05:27 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,6 @@
 input.onGesture(Gesture.Shake, function () {
     music.playMelody("C5 G B A F A C5 B ", 188)
 })
-input.onPinPressed(TouchPin.P1, function () {
-	
-})
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     if (input.pinIsPressed(TouchPin.P1)) {
         basic.showLeds(`
